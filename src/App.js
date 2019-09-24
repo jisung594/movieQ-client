92b36c46b2336706499ed20b5670ebd0e2baf7cf
Jonathan Choi <jisung594@gmail.com>
2019-09-23 23:01:07 -0400

    debugging backend connection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Styling/App.scss';
 
 class App extends Component {
+  state = {
+    users: [],
+    movies: []
+  }
+
+  componentDidMount(){
+    this.fetchUsers()
+      .then(res => this.setState({
+        users: res
+      }))
+      .catch(err => console.log(err))
+
+    this.fetchMovies()
+      .then(res => this.setState({
+        users: res
+      }))
+      .catch(err => console.log(err))
+  }
+
+  fetchUsers = async () => {
+    const response = await fetch('/users')
+    const body = await response.json()
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+
+    return body
+  }
+
+  fetchMovies = async () => {
+    const response = await fetch('/api/movies')
+    const body = await response.json()
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+
+    return body
+  }
+
   render() {
     return (
       <div className="App">
-
+        { console.log(this.state.users ? this.state.users : null) }
+        { console.log(this.state.movies ? this.state.movies : null) }
       </div>
     );
   }
